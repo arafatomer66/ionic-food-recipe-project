046f7d750a5b70374a4971a139919eab7113fdc0
Your Name <you@example.com>
2019-08-07 02:36:29 -0800

    coint ..

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,12 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  //  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  //  {
+  //            path : 'home' ,
+  //            loadChildren: './home/home.module#HomePageModule' ,
+  //          }
+
   { path: '', redirectTo: 'recipes', pathMatch: 'full' },
 
   { path: 'recipes', children : [
